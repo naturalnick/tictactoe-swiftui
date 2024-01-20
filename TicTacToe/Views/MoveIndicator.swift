0efa1560defc0a568f59629eedb940a5b039da74
Nick Schaefer <wildernns@gmail.com>
2024-01-20 12:35:42 -0500

    Remove reverse wild game type, fix grid item zindex on drag, refactoring

---
+++ TicTacToe/Views/MoveIndicator.swift
@@ -0,0 +1,18 @@
+//
+//  MoveIndicator.swift
+//  TicTacToe
+//
+//  Created by Nick Schaefer on 1/19/24.
+//
+
+import SwiftUI
+
+struct MoveIndicator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoveIndicator()
+}
